1f8a7f6828f2fab99204110f80827684991cf7fa
meowy <meowy@meowerity.com>
2026-01-17 00:25:34 +0900

    Getting started...

--- main.cpp
+++ main.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+#include <spdlog/spdlog.h>
+using namespace ImGui;
+
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    bool Test_CheckBox = false;
+    int counter = 0;
+
+    sf::RenderWindow window(
+        sf::VideoMode({800, 600}),
+        "Meowy Engine V1"
+    );
+
+    //Init ImGUI
+    SFML::Init(window);
+
+    sf::Clock deltaClock;
+
+    while (window.isOpen())
+    {
+        while (auto event = window.pollEvent())
+        {
+            SFML::ProcessEvent(window, *event);
+
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+
+        }
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        // Default window
+
+        Begin("Welcome to Meowy Engine :3");
+        Text("Welcome :333");
+        if (Button("Counter +"))
+        {
+            counter++;
+        }
+
+        Text("Counter: %d", counter);
+
+        End();
+
+        window.clear(sf::Color::White);
+
+        SFML::Render(window);
+
+        window.display();
+    }
+
 }
